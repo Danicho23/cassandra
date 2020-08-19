657db22cf61c9ee3262b95d62ef4e66e5b9cb6fc
WillianPanza11 <57450978+WillianPanza11@users.noreply.github.com>
2020-08-19 14:34:47 -0500

    Target

--- FrontEnd/EjemploNoSQL/src/app/app.routes.ts
+++ FrontEnd/EjemploNoSQL/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 
 import { RouterModule, Routes } from '@angular/router';
 import { BodyComponent } from './components/body/body.component';
+import { TarjetaPersonaComponent } from './components/tarjeta-persona/tarjeta-persona.component';
+import { TarjetaVehiculosComponent } from './components/tarjeta-vehiculos/tarjeta-vehiculos.component';
+
 const ROUTES: Routes = [
-    { path: 'home', component: BodyComponent}
+    { path: 'home', component: BodyComponent},
+    { path: 'listPerson', component: TarjetaPersonaComponent},
+    { path: 'listCars', component: TarjetaVehiculosComponent}
 ];
 
 export const APPROUTING = RouterModule.forRoot(ROUTES);

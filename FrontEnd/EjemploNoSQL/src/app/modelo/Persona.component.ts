4f828023524871b457fb87ef3d3ebe2e300623c8
fxvr15vasquez <57450853+fxvr15vasquez@users.noreply.github.com>
2020-08-19 11:37:35 -0500

    modelo

---
+++ FrontEnd/EjemploNoSQL/src/app/modelo/Persona.component.ts
@@ -0,0 +1,7 @@
+export interface Persona {
+    id: number;
+    cedula: string;
+    nombre: string;
+    apellido: string;
+    edad: number;
+}
---
+++ FrontEnd/EjemploNoSQL/src/app/modelo/Vehiculo.components.ts
@@ -0,0 +1,9 @@
+export interface Vehiculo {
+    id: number;
+    marca: string;
+    modelo: string;
+    colorP: string;
+    colorS: string;
+    edad: number;
+    anioFabri: Date;
+}
